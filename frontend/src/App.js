9c30cb2210c4046217c7dc350968b2a04b26fa25
Tom Leb <tomleb3@gmail.com>
2021-01-13 11:31:02 +0200

    FIRST

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { routes } from './routes.js'
+import { AppHeader } from './cmps/AppHeader.jsx'
+import { Switch, Route } from 'react-router-dom'
+import './styles/styles.scss'
+
+function App() {
+  return (
+    <main>
+      <AppHeader />
+      <Switch>
+        {routes.map(route => <Route key={route.path} exact component={route.component} path={route.path} />)}
+      </Switch>
+    </main>
+  );
+}
+
+export default App;
