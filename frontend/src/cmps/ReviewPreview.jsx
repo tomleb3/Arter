89fa15f8e44d26f414e58c7f69f89d52c0336875
eranhh <eranhouja@gmail.com>
2021-01-14 14:29:18 +0200

    thursday 14;30

--- frontend/src/cmps/ReviewPreview.jsx
+++ frontend/src/cmps/ReviewPreview.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import { UserPreview } from './UserPreview';
+// import { LongTxt } from './LongTxt';
 
-export default function reviewPreview() {
-    return (
-        <div>
-            
-        </div>
-    )
+export function ReviewPreview({ review }) {
+    return (
+        <div className="review-preview">
+            <UserPreview user={review.by} date={review.createdAt}/>
+            <StyledRating value={review.rate} readOnly />
+{/*             <LongTxt length={120} txt={review.txt}/>      */}
+        </div>
+    )
 }
+
+const StyledRating = withStyles({ iconFilled: { color: '$clr1' } })(Rating);
+
+// export default function reviewPreview() {
+//     return (
+//         <div>
+            
+//         </div>
+//     )
+// }
