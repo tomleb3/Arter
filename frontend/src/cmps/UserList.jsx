c2d3ccb84cae03dc8441c624617001f3c89bc4b1
Tom Leb <tomleb3@gmail.com>
2021-01-14 22:23:29 +0200

    thu_22:23

--- frontend/src/cmps/UserList.jsx
+++ frontend/src/cmps/UserList.jsx
@@ -2,28 +2,65 @@ import { UserPreview } from "./UserPreview.jsx"
 import Carousel from 'react-material-ui-carousel'
 import { Paper, Button } from '@material-ui/core'
 
-export function UserList({ items, users }) {
-    return (
-        <Carousel>
-                {users && users.map(user => {
-                    return <UserPreview key={user._id} user={user} />
-                })}
-        </Carousel>
-    )
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+// SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
+export function UserList({ users, items }) {
+    return <Swiper
+        className="user-list main-layout"
+        spaceBetween={30}
+        slidesPerView={5}
+        navigation
+        Autoplay
+        EffectFade
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}>
+
+        {users && users.map(user => {
+            return <SwiperSlide>
+                <UserPreview key={user._id} user={user} />
+            </SwiperSlide>
+        })}
+        {/* <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+... */}
+    </Swiper>
 }
 
-function Item(props) {
-    return (
-        <Paper>
-            <h2>{props.user.title}</h2>
-            <p>{props.user.description}</p>
 
-            <Button className="CheckButton">
-                Check it out!
-            </Button>
-        </Paper>
-    )
-}
+
+// export function UserList({ items, users }) {
+//     return (
+//         <Carousel>
+//             {users && users.map(user => {
+//                 return <UserPreview key={user._id} user={user} />
+//             })}
+//         </Carousel>
+//     )
+// }
+
+// function Item(props) {
+//     return (
+//         <Paper>
+//             <h2>{props.user.title}</h2>
+//             <p>{props.user.description}</p>
+
+//             <Button className="CheckButton">
+//                 Check it out!
+//             </Button>
+//         </Paper>
+//     )
+// }
 
 
 
