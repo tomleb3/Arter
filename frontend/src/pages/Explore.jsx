0ddfa0aa75d9811c15f302261fd5f15f31d22503
eranhh <eranhouja@gmail.com>
2021-01-14 15:26:06 +0200

    filter attempts

--- frontend/src/pages/Explore.jsx
+++ frontend/src/pages/Explore.jsx
@@ -1,13 +1,34 @@
 import { Component } from 'react'
 import { UserList } from '../cmps/UserList.jsx'
 import { ItemList } from '../cmps/ItemList.jsx'
+import {AppFilter} from '../cmps/AppFilter'
+import { loadItems } from '../store/actions/itemActions'
+import { connect } from 'react-redux'
 
-export class Explore extends Component {
+class _Explore extends Component {
+
+    onSetFilter = (filterBy) => {
+        this.props.loadItems(filterBy)
+    }
 
     render() {
         return <section className="explore">
+            <AppFilter onSetFilter={this.onSetFilter} />
             <UserList />
             <ItemList />
         </section>
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+      // loggedInUser: state.userModule.loggedInUser
+    }
+  }
+  
+  const mapDispatchToProps = {
+    loadItems
+    // loadUsers
+  }
+  
+  export const Explore = connect(mapStateToProps, mapDispatchToProps)(_Explore)
