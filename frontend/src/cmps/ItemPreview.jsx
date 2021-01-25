68ac8ac4c72c6d2548dab8e1b7deb9b88c2596c3
Tom Leb <tomleb3@gmail.com>
2021-01-25 12:26:08 +0200

    further_responsive_stuff

--- frontend/src/cmps/ItemPreview.jsx
+++ frontend/src/cmps/ItemPreview.jsx
@@ -1,11 +1,10 @@
 import { Fragment } from 'react'
-import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { utilService } from '../services/utilService.js'
-import StarIcon from '@material-ui/icons/Star';
-// import { Button } from '@material-ui/core'
+import StarIcon from '@material-ui/icons/Star'
+// import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
 
-function _ItemPreview({ item, withProfile, minified }) {
+export function ItemPreview({ item, withProfile, minified }) {
     const userRating = utilService.calcRate(item.seller)
 
     if (minified) return <section className="item-preview min">
@@ -23,26 +22,22 @@ function _ItemPreview({ item, withProfile, minified }) {
 
     return <section className="item-preview">
         <Link to={`/item/${item._id}`}><img className="item-img" src={item.imgUrl} alt="" /></Link>
-        <div className="item-info">
-            {withProfile && <div className="flex j-between">
-                <div className="flex">
-                    <Link to={`/user/${item.seller._id}`} className="user-name flex a-center"><p>{item.seller.fullname}</p></Link>
-                    {userRating &&
-                        <Fragment>
-                            <StarIcon color="secondary" readOnly fontSize="small" />
-                            <p className="user-rating font-mont">{userRating}</p>
-                        </Fragment>}
-                </div>
-            </div>}
-            <Link to={`/item/${item._id}`}><h3>{item.title}</h3></Link>
-            {item.purchasedAt ? <p className="site-clr3">SOLD</p> : <p className="font-mont">${item.price}</p>}
+        <div className="flex j-between">
+            <div className="item-info">
+                {withProfile && <div className="flex j-between">
+                    <div className="flex">
+                        <Link to={`/user/${item.seller._id}`} className="user-name flex a-center"><p>{item.seller.fullname}</p></Link>
+                        {userRating &&
+                            <Fragment>
+                                <StarIcon color="secondary" readOnly fontSize="small" />
+                                <p className="user-rating font-mont">{userRating}</p>
+                            </Fragment>}
+                    </div>
+                </div>}
+                <Link to={`/item/${item._id}`}><h3>{item.title}</h3></Link>
+                {item.purchasedAt ? <p className="site-clr3">SOLD</p> : <p className="font-mont">${item.price}</p>}
+            </div>
+            {/* <FavoriteBorderIcon className="fav-icon pointer site-clr3" fontSize="" /> */}
         </div>
     </section>
 }
-
-const mapStateToProps = state => {
-    return {
-        // loggedInUser: state.userModule.loggedInUser
-    }
-}
-export const ItemPreview = connect(mapStateToProps)(_ItemPreview)
